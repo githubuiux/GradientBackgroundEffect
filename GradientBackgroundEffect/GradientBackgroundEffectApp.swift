f5c5af5339325ed5ae8b537decd82f58e5a191fa
githubuiux <uiuxista@gmail.com>
2023-04-25 16:27:15 +0300

    Initial Commit

---
+++ GradientBackgroundEffect/GradientBackgroundEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientBackgroundEffectApp.swift
+//  GradientBackgroundEffect
+//
+//  Created by Levent on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GradientBackgroundEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
